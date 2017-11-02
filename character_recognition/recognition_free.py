78ec24c4cc5863ff43344bdf3a81fe7ec69e63c3
wada-don <rw.ryu.18.wr@gmail.com>
2017-11-02 13:48:54 +0900

    change dir construction

--- character_recognition/recognition_free.py
+++ character_recognition/recognition_free.py
@@ -177,13 +177,16 @@ if not os.path.exists('recog_images/pro_img/{}'.format(image_name)):
   os.mkdir('recog_images/pro_img/{}'.format(image_name))
 
 #画像内から文字が記述されている範囲を特定するためのループ
-for cropped_width in range(100/2, 300/2, 20/2):
-    for cropped_height in range(100/2, 300/2, 20/2):
+for cropped_width in range(100/2, 300/2, 10):
+    for cropped_height in range(100/2, 300/2, 10):  #ここを固定にしたい
         for shift_x in range(0, width-cropped_width, cropped_width/4):
             for shift_y in range(0, height-cropped_height, cropped_height/4):
 
                 #特定のサイズに切り出す
                 gray = image[shift_y:shift_y+cropped_height,shift_x:shift_x + cropped_width]
+
+                if not os.path.exists('recog_images/pro_img/{}/crop'.format(image_name)):
+                  os.mkdir('recog_images/pro_img/{}/crop'.format(image_name))
 
                 #色がついている部分がなければ次の範囲へ
                 if np.count_nonzero(gray) <= 40:
