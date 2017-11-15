9060548745bdfad5b7f7729c0417d0a3ba6282e3
Kenji Fujiwara <niyaton@gmail.com>
2017-11-15 22:17:50 +0900

    プログラム引数を使用するようにした

--- create_image/conversion_ETL3.py
+++ create_image/conversion_ETL3.py
@@ -20,8 +20,13 @@ def main(in_file, out_file):
         raise
 
 if __name__ == '__main__':
-    in_file = open('ETL3/ETL3C_2', 'rb')  #change ETL3C_1 to ETL3C_2 in need
-    out_file = open('ETL3/ETL3C_2_converted', 'wb+')
+    import sys
+    if len(sys.argv) != 3:
+        print("Usage: python {} <in_file> <out_file>".format(sys.argv[0]))
+        sys.exit(1)
+        
+    in_file = open(sys.argv[1], 'rb')
+    out_file = open(sys.argv[2], 'wb+')
 
     main(in_file, out_file)
 
