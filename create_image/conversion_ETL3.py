d8fee561788290e0e98a1c9406928e47340c272b
Kenji Fujiwara <niyaton@gmail.com>
2017-11-15 22:01:09 +0900

    処理を整理しました

--- create_image/conversion_ETL3.py
+++ create_image/conversion_ETL3.py
@@ -1,40 +1,23 @@
 def main(in_file, out_file):
     bits = [b'\x00', b'\x30', b'\x3c', b'\x3f']
+    bits = [int.from_bytes(i, 'little') for i in bits]
 
-    read_data = [b'\x00']
+    try:
+        for num in range(73):  #72*3=216byte
+            data = [0]
+            data.extend(in_file.read(3))
+            data.append(0)
 
-    while True:
-        if not read_data:
-            break
-
-        for num in range(72):  #72*3=216byte
-            data = [b'\x00']
-
-            for j in range(3):  #read 3bytes
-                read_data = in_file.read(1)
-                if not read_data:
-                    break
-
-                data.append(read_data)
-
-            if not read_data:
-                break
-
-            data.append(b'\x00')
-
-            for i in range(4):
-                a = int.from_bytes(data[i], 'little')
-                b = int.from_bytes(data[i+1], 'little')
-
-                d = ((a<<(6-(i*2))) & int.from_bytes(bits[i], 'little')) | (b>>(i*2+2)) 
+            for lhs, mask, rhs in zip(data, bits, data[1:]):
+                d = ((lhs<<(6-(i*2))) & mask) | (rhs>>(i*2+2))
 
                 out_file.write(d.to_bytes(1, 'little'))
 
-        if not read_data:
-            break
-
         read_data = in_file.read(2736)
         out_file.write(read_data)
+    except IOError:
+        print("cannot read data")
+        raise
 
 if __name__ == '__main__':
     in_file = open('ETL3/ETL3C_2', 'rb')  #change ETL3C_1 to ETL3C_2 in need
