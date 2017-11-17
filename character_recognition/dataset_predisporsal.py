840668eb8bc9d658340872af1c8a90154a5ff624
wada-don <rw.ryu.18.wr@gmail.com>
2017-11-17 13:17:14 +0900

    fix-predisporsal first commit

--- character_recognition/dataset_predisporsal.py
+++ character_recognition/dataset_predisporsal.py
@@ -1,35 +1,48 @@
 import os
 import numpy as np
 import cv2 as cv
+import gc
 
-data_dir_path = "dataset_images/old"
+data_dir_path = "dataset_images/new/"
 tmp = os.listdir(data_dir_path)
 tmp=sorted([x for x in tmp if os.path.isdir(data_dir_path+x)])
 dir_list = tmp
 
-X_target=[]
+X_target=np.empty((0, 3, 32, 32), dtype=np.uint8)
+c_class=[]
+
 for dir_name in dir_list:
+    print(dir_name)
     file_list = os.listdir(data_dir_path+dir_name)
     for file_name in file_list:
         if file_name.endswith('.png'):
             image_path=str(data_dir_path)+str(dir_name)+'/'+str(file_name)
+            # print('1')
             image = cv.imread(image_path)
-            image = cv.resize(image, (32, 32))
-            image = image.transpose(2,0,1)
+            # print('2')
+            image = cv.resize(image, (32, 32))  #[32][32][3]
+            # print('3')
+            image = image.transpose(2,0,1)  #[3][32][32]
+            # print('4')
             image = image/255.
-            X_target.append(image)
-
-c_class=[]
-
-count=0
-for dir_name in dir_list:
-    file_list = os.listdir(data_dir_path+dir_name)
-    for file_name in file_list:
-        if file_name.endswith('.png'):
+            # print('5')
+            X_tmp = np.array([image])
+            # print('6')
+            X_target = np.vstack((X_target, X_tmp))
+            # print('7')
             c_class.append(dir_name)
-    #count+=1
+            # print(file_name)
 
-c_arr2=np.array(c_class)
-np.save('character_label.npy',c_arr2)
-c_arr=np.array(X_target)
-np.save('character_data.npy',c_arr)
+
+if not os.path.exists('npy_data'):
+    os.mkdir('npy_data')
+
+c_arr=np.array(c_class)
+np.save('npy_data/character_label_new.npy',c_arr)
+
+try:
+    np.save('npy_data/character_data_new.npy',X_target)
+except Memoryeror as e:
+    print(e)
+
+
