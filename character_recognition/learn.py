1ff318b2345143011c79f15fcdafcdc04e4bf842
wada-don <rw.ryu.18.wr@gmail.com>
2017-11-17 13:15:49 +0900

    change dropout

--- character_recognition/learn.py
+++ character_recognition/learn.py
@@ -23,7 +23,7 @@ model.add(Activation('relu'))
 
 model.add(Conv2D(128, 3, 3))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(1024))
