021c5f424db6c2e8f89185b3d00a500ecc42a355
wada-don <rw.ryu.18.wr@gmail.com>
2017-12-06 13:47:34 +0900

    fix all issue in predisporsal

--- character_recognition/dataset_predisporsal.py
+++ character_recognition/dataset_predisporsal.py
@@ -8,30 +8,38 @@ tmp = os.listdir(data_dir_path)
 tmp=sorted([x for x in tmp if os.path.isdir(data_dir_path+x)])
 dir_list = tmp
 
-X_target=np.empty((0, 3, 32, 32), dtype=np.uint8)
-c_class=[]
+X_target = np.empty((0, 3, 32, 32), dtype=np.uint8)
+c_class = []
 
 for dir_name in dir_list:
     print(dir_name)
     file_list = os.listdir(data_dir_path+dir_name)
+
+    png_count = 0
+    for f in file_list:
+        if f.endswith('.png'):
+            png_count+=1
+
+    print(png_count)
+
+    X_tmp = np.empty((png_count, 3, 32, 32), dtype=np.uint8)
+
+    png_count = 0
     for file_name in file_list:
         if file_name.endswith('.png'):
             image_path=str(data_dir_path)+str(dir_name)+'/'+str(file_name)
-            # print('1')
             image = cv.imread(image_path)
-            # print('2')
             image = cv.resize(image, (32, 32))  #[32][32][3]
-            # print('3')
             image = image.transpose(2,0,1)  #[3][32][32]
-            # print('4')
             image = image/255.
-            # print('5')
-            X_tmp = np.array([image])
-            # print('6')
-            X_target = np.vstack((X_target, X_tmp))
-            # print('7')
+            tmp = np.array([image])
+            X_tmp[png_count] = tmp
+            png_count+=1
+
             c_class.append(dir_name)
             # print(file_name)
+
+    X_target = np.vstack((X_target, X_tmp))
 
 
 if not os.path.exists('npy_data'):
@@ -42,7 +50,7 @@ np.save('npy_data/character_label_new.npy',c_arr)
 
 try:
     np.save('npy_data/character_data_new.npy',X_target)
-except Memoryeror as e:
+except MemoryError as e:
     print(e)
 
 
