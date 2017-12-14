5b209cfce8cff46f1316fb4683377d7dda121f90
wada-don <rw.ryu.18.wr@gmail.com>
2017-12-14 12:02:41 +0900

    refacotring dataset_predisporsal.py

--- character_recognition/dataset_predisporsal.py
+++ character_recognition/dataset_predisporsal.py
@@ -3,22 +3,36 @@ import numpy as np
 import cv2 as cv
 import gc
 
-data_dir_path = "dataset_images/new/"
-tmp = os.listdir(data_dir_path)
-tmp=sorted([x for x in tmp if os.path.isdir(data_dir_path+x)])
-dir_list = tmp
+def get_dir_list():
+  data_dir_path = "dataset_images/new/"
+  dir_list = os.listdir(data_dir_path)
+  dir_list = sorted([x for x in tmp if os.path.isdir(data_dir_path+x)])
 
-X_target = np.empty((0, 3, 32, 32), dtype=np.uint8)
-c_class = []
+  return (data_dir_path, dir_list)
 
-for dir_name in dir_list:
+
+def image_processing(image_path):
+  image = cv.imread(image_path)
+  image = cv.resize(image, (32, 32))  #[32][32][3]
+  image = image.transpose(2,0,1)  #[3][32][32]
+  image = image/255.
+
+  return image
+
+
+def make_target_and_class(data_dir_path, dir_list):
+  X_target = np.empty((0, 3, 32, 32), dtype=np.uint8)
+  c_class = []
+
+  for dir_name in dir_list:
     print(dir_name)
     file_list = os.listdir(data_dir_path+dir_name)
 
     png_count = 0
+
     for f in file_list:
-        if f.endswith('.png'):
-            png_count+=1
+      if f.endswith('.png'):
+        png_count+=1
 
     print(png_count)
 
@@ -26,31 +40,36 @@ for dir_name in dir_list:
 
     png_count = 0
     for file_name in file_list:
-        if file_name.endswith('.png'):
-            image_path=str(data_dir_path)+str(dir_name)+'/'+str(file_name)
-            image = cv.imread(image_path)
-            image = cv.resize(image, (32, 32))  #[32][32][3]
-            image = image.transpose(2,0,1)  #[3][32][32]
-            image = image/255.
-            tmp = np.array([image])
-            X_tmp[png_count] = tmp
-            png_count+=1
+      if file_name.endswith('.png'):
+        image_path = os.path.join(str(data_dir_path)+str(dir_name),str(file_name))
+        image = image_processing(image_path)
+        X_tmp[png_count] = np.array([image])
+        png_count+=1
 
-            c_class.append(dir_name)
-            # print(file_name)
+        c_class.append(dir_name)
+        # print(file_name)
 
     X_target = np.vstack((X_target, X_tmp))
 
+  return (c_class, X_target)
 
-if not os.path.exists('npy_data'):
+
+def main():
+  data_dir_path, dir_list = get_dir_list()
+
+  c_class, X_target = make_target_and_class(data_dir_path, dir_list)
+
+  if not os.path.exists('npy_data'):
     os.mkdir('npy_data')
 
-c_arr=np.array(c_class)
-np.save('npy_data/character_label_new.npy',c_arr)
+  c_arr=np.array(c_class)
+  np.save('npy_data/character_label_new.npy',c_arr)
 
-try:
+  try:
     np.save('npy_data/character_data_new.npy',X_target)
-except MemoryError as e:
+  except MemoryError as e:
     print(e)
 
 
+if __name__ == '__main__':
+  main()
